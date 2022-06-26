76d533741b2fd79541b5471cbe6eb34642617c78
Jonathan "JonLem" Lemos <jonlemofficial@hotmail.com>
2022-06-26 12:39:34 -0400

    Fixing jwt cookie domain name

--- app/router.js
+++ app/router.js
@@ -95,7 +95,7 @@ apiRouter.post("/login", (req, res) => {
       res.cookie('jwt', refreshToken, {
         httpOnly: true,
         path: '/',
-        domain: isProd ? 'https://unergapp.herokuapp.com' : 'http://localhost:3000',
+        domain: isProd ? 'unergapp.herokuapp.com' : 'localhost:3000',
         ...isProd ? { sameSite: 'None' } : {},
         secure: isProd ? true : false,
         maxAge: 1000 * 60 * 60 * 90  // 90 days
